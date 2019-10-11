b4043127a710249ca9273e72e0aeb614180d3025
mikyUtad <miguel1603mg@gmail.com>
2019-10-11 12:28:37 +0200

    cambios en el main

--- app/src/main/java/es/sarascript/blackjack/MainActivity.kt
+++ app/src/main/java/es/sarascript/blackjack/MainActivity.kt
@@ -1,9 +1,11 @@
 
 package es.sarascript.blackjack
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +18,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnPlayer2: Button
     private lateinit var btnStopPlayer1: Button
     private lateinit var btnStopPlayer2: Button
+    private lateinit var dice1: ImageView
+    private lateinit var dice2: ImageView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,13 +28,23 @@ class MainActivity : AppCompatActivity() {
 
         txtPlayer1 = findViewById(R.id.j1_txt)
         txtResultPlayer1= findViewById(R.id.score1_txt)
+        dice1 = findViewById(R.id.dice1_img)
         txtPlayer2 = findViewById(R.id.j2_txt)
         txtResultPlayer2 = findViewById(R.id.score2_txt)
 
         btnPlayer1 = findViewById(R.id.j1_btn)
         btnStopPlayer1= findViewById(R.id.stop1_btn)
+        dice2 = findViewById(R.id.dice2_img)
         btnPlayer2 = findViewById(R.id.j2_btn)
         btnStopPlayer2 = findViewById(R.id.stop2_btn)
+
+        var contPlayer1 = 0
+        var contPlayer2 = 0
+
+        fun randomNumber(): Int {
+            val random = (1..6).shuffled().first()
+            return random
+        }
 
 
     }
